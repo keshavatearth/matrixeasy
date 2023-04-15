08895b6d16d2db7a057b6959e62a48d240448ee9
Keshav Jindal <97098933+keshavatearth@users.noreply.github.com>
2023-04-15 22:13:37 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,11 +1,12 @@
 //function to generate empty input matrix based on dimensions. MatrixId is defined when you call out the function and name the rows and cols input accordingly.
 
 function generateMatrixInput(matrixId) {
-  
+    prevMatrix = document.getElementById("inputBox"+matrixId);
+    prevMatrix.innerHTML = "";
     let rows = parseInt(document.getElementById("rows" + matrixId).value);
     let cols = parseInt(document.getElementById("cols" + matrixId).value);
     let matrixInput = document.createElement("div");
-  matrixInput.id = "input"+matrixId;
+    matrixInput.id = "input" + matrixId;
     matrixInput.innerHTML = "";
     for (let i = 0; i < rows; i++) {
         let rowDiv = document.createElement("div");
@@ -19,10 +20,5 @@ function generateMatrixInput(matrixId) {
         matrixInput.appendChild(rowDiv);
     }
     matrixInput.appendChild(document.createElement("br"));
-    document.getElementById("inputValues").appendChild(matrixInput);
-    let prevMatrixInput = document.getElementById("input" + matrixId);
-    if (prevMatrixInput) {
-        inputValues.removeChild(prevMatrixInput);
-    }
-    inputValues.appendChild(matrixInput)
+    document.getElementById("inputBox"+matrixId).appendChild(matrixInput);
 }
